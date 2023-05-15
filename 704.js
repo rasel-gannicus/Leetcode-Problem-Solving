06e179747ee6f47c0a4a4fd494d54dd3f1b38f71
shafik720 <shafikrasel5@gmail.com>
2023-05-15 19:42:13 +0600

    2nd problem on leetcode solved

--- 704.js
+++ 704.js
@@ -4,25 +4,30 @@
 const dummyArray = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 110, 120];
 
 
-const binarySearch = (arr, target) =>{
-    let start = 0 ;
-    let end = arr.length - 1 ;
-    let mid ;
+const binarySearch = (arr, target) => {
+    let start = 0;
+    let end = arr.length - 1;
+    let mid;
 
-    while(start <= end){
-        console.log('Started Searching') ; 
-        mid = Math.floor((start + end)/2) ;
-        
-        if(arr[mid] === target) {
-            return console.log('Data found at ', mid) ; 
+    while (start <= end) {
+        let start = 0;
+        let end = arr.length - 1;
+        let mid;
+
+        while (start <= end) {
+            mid = Math.floor((start + end) / 2);
+
+            if (arr[mid] === target) {
+                return mid;
+            }
+            else if (arr[mid] < target) {
+                start = mid + 1;
+            } else if (arr[mid] > target) {
+                end = mid - 1
+            }
         }
-        else if(arr[mid] < target){
-            start = mid + 1 ; 
-        }else if(arr[mid] > target){
-            end = mid -1
-        }
+        return -1;
     }
-    return console.log('No data found');
 }
 
-binarySearch(dummyArray, 100) ;
+binarySearch(dummyArray, 100);
